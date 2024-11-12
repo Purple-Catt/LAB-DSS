b9cc9f0520903ddea34e74a72358e4ee3cdb4c2f
Marco <marcodelpi@hotmail.com>
2024-11-12 10:58:14 +0100

    Minor upgrades

--- MARCO_Task1.py
+++ MARCO_Task1.py
@@ -1,9 +1,12 @@
 import pandas as pd
-import numpy as np
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 people = pd.read_csv(ROOT_DIR + '/Data/People.csv', header=0)
 crashes = pd.read_csv(ROOT_DIR + '/Data/Crashes.csv', header=0)
 vehicles = pd.read_csv(ROOT_DIR + '/Data/Vehicles.csv', header=0)
+# print the number of missing values in each dataset
+# The way to deal with them is described in 'handling_missing_values.txt'
 print(people.isna().sum())
+print(crashes.isna().sum())
+print(vehicles.isna().sum())
---
+++ MARCO_Task2.py
@@ -0,0 +1,64 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+people = open(ROOT_DIR + '/Data/PeopleCopy.csv', 'r')
+crashes = open(ROOT_DIR + '/Data/CrashesCopy.csv', 'r')
+vehicles = open(ROOT_DIR + '/Data/VehiclesCopy.csv', 'r')
+
+people_lines = people.readlines()
+crashes_lines = crashes.readlines()
+vehicles_lines = vehicles.readlines()
+
+people.close()
+crashes.close()
+vehicles.close()
+
+people = open(ROOT_DIR + '/Data/PeopleCopy.csv', 'w')
+crashes = open(ROOT_DIR + '/Data/CrashesCopy.csv', 'w')
+vehicles = open(ROOT_DIR + '/Data/VehiclesCopy.csv', 'w')
+
+people_columns = people_lines.pop(0).split(',')
+crashes_columns = crashes_lines.pop(0).split(',')
+vehicles_columns = vehicles_lines.pop(0).split(',')
+
+for idx, line in enumerate(people_lines):
+    split_line = line.split(',')
+    # VEHICLE_ID null means that the person is PEDESTRIAN, BICYCLE or NON-MOTOR VEHICLE, so it can be labelled as 0
+    if split_line[people_columns.index('VEHICLE_ID')] == '':
+        split_line[people_columns.index('VEHICLE_ID')] = '0'
+
+    # SEX null values can be labelled as X
+    if split_line[people_columns.index('SEX')] == '':
+        split_line[people_columns.index('SEX')] = 'X'
+
+    # SAFETY_EQUIPMENT null values can be labelled as USAGE UNKNOWN
+    if split_line[people_columns.index('SAFETY_EQUIPMENT')] == '':
+        split_line[people_columns.index('SAFETY_EQUIPMENT')] = 'USAGE UNKNOWN'
+
+    # AIRBAG_DEPLOYED null values can be labelled as DEPLOYMENT UNKNOWN
+    if split_line[people_columns.index('AIRBAG_DEPLOYED')] == '':
+        split_line[people_columns.index('AIRBAG_DEPLOYED')] = 'DEPLOYMENT UNKNOWN'
+
+    # EJECTION null values can be labelled as UNKNOWN
+    if split_line[people_columns.index('EJECTION')] == '':
+        split_line[people_columns.index('EJECTION')] = 'UNKNOWN'
+
+    # DRIVER_ACTION null values can be labelled as UNKNOWN
+    if split_line[people_columns.index('DRIVER_ACTION')] == '':
+        split_line[people_columns.index('DRIVER_ACTION')] = 'UNKNOWN'
+
+    # DRIVER_VISION null values can be labelled as UNKNOWN
+    if split_line[people_columns.index('DRIVER_VISION')] == '':
+        split_line[people_columns.index('DRIVER_VISION')] = 'UNKNOWN'
+
+    # PHYSICAL_CONDITION null values can be labelled as UNKNOWN
+    if split_line[people_columns.index('PHYSICAL_CONDITION')] == '':
+        split_line[people_columns.index('PHYSICAL_CONDITION')] = 'UNKNOWN'
+
+    people_lines[idx] = ','.join(split_line)
+
+people.writelines(people_lines)
+
+people.close()
+crashes.close()
+vehicles.close()
