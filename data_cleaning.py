e92faa6c4334099f7eb73c8a44ebbac3f39e0000
Marco <marcodelpi@hotmail.com>
2025-01-19 13:55:55 +0100

    Task 2 (final) commit

---
+++ data_cleaning.py
@@ -0,0 +1,104 @@
+import csv
+from datetime import datetime, timedelta
+
+columns_to_drop = ['winner_seed', 'winner_entry', 'winner_ht', 'loser_seed', 'loser_entry', 'loser_ht', 'minutes',
+                   'w_ace', 'w_df', 'w_svpt', 'w_1stIn', 'w_1stWon', 'w_2ndWon', 'w_SvGms', 'w_bpSaved', 'w_bpFaced',
+                   'l_ace', 'l_df', 'l_svpt', 'l_1stIn', 'l_1stWon', 'l_2ndWon', 'l_SvGms', 'l_bpSaved', 'l_bpFaced',
+                   'winner_rank', 'winner_rank_points', 'loser_rank', 'loser_rank_points']
+'''
+winner_age, winner_hand, loser_age, loser_hand will be substituted with the median
+Drop winner and loser ioc (?), score
+'''
+fact_dict_stats = {'winner_age': [], 'winner_hand': {'A': 0, 'U': 0, 'L': 0, 'R': 0},
+                   'loser_age': [], 'loser_hand': {'A': 0, 'U': 0, 'L': 0, 'R': 0}}
+with open('Data\\fact.csv', newline='') as file:
+    reader = csv.reader(file)
+    var = True
+    for row in reader:
+        if var:
+            var = False
+            col_idx_fact = dict()
+            col_idx_to_drop = list()
+            for k, v in enumerate(row):
+                col_idx_fact[v] = k
+                if v in columns_to_drop:
+                    col_idx_to_drop.append(k)
+            continue
+
+        for k in fact_dict_stats.keys():
+            if k in ['winner_age', 'loser_age']:
+                try:
+                    fact_dict_stats[k].append(float(row[col_idx_fact[k]]))
+                except ValueError:
+                    pass
+
+            else:
+                value = row[col_idx_fact[k]]
+                if value != '':
+                    fact_dict_stats[k][value] += 1
+
+# Compute the average for numerical values, mode for categorical ones
+for k in fact_dict_stats.keys():
+    if k in ['winner_age', 'loser_age']:
+        fact_dict_stats[k] = round(sum(fact_dict_stats[k]) / len(fact_dict_stats[k]), 1)
+
+    else:
+        fact_dict_stats[k] = sorted(fact_dict_stats[k].items(), key=lambda x: x[1], reverse=True)[0][0]
+
+
+with open('Data\\fact.csv', newline='') as r_file:
+    with open('Data\\fact_cleaned.csv', newline='', mode='w') as w_file:
+        reader = csv.reader(r_file)
+        writer = csv.writer(w_file)
+        var = True
+        for row in reader:
+            if var:
+                var = False
+                for idx in reversed(col_idx_to_drop):
+                    row.pop(idx)
+                writer.writerow(row)
+                continue
+
+            for k in fact_dict_stats.keys():
+                if row[col_idx_fact[k]] == '':
+                    row[col_idx_fact[k]] = fact_dict_stats[k]
+
+            for idx in reversed(col_idx_to_drop):
+                row.pop(idx)
+
+            writer.writerow(row)
+
+with open('Data\\tourney.csv', newline='') as r_file:
+    with open('Data\\tourney_cleaned.csv', newline='', mode='w') as w_file:
+        reader = csv.reader(r_file)
+        writer = csv.writer(w_file)
+        var = True
+        for row in reader:
+            if var:
+                var = False
+                col_idx_tourney = dict()
+                for k, v in enumerate(row):
+                    col_idx_tourney[v] = k
+
+                row.pop(-1)
+                row.append('day')
+                row.append('month')
+                row.append('year')
+                writer.writerow(row)
+                continue
+
+            if row[col_idx_tourney['surface']] == '':
+                row[col_idx_tourney['surface']] = 'Clay'
+
+            if row[col_idx_tourney['draw_size']] == '':
+                row[col_idx_tourney['draw_size']] = 32
+
+            dt = datetime(1970, 1, 1) + timedelta(
+                seconds=int(row.pop(col_idx_tourney['tourney_timestamp'])))
+            row.append(str(dt.day))
+            row.append(str(dt.month))
+            row.append(str(dt.year))
+
+            writer.writerow(row)
+
+
